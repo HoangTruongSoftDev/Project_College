4e596f07c0a6f1e237403a5b3116ba55a5b930ef
HoangTruongSoftDev <hoangtruongpham73@gmail.com>
2023-09-05 14:30:16 -0400

    adding Android Project

--- JavaScript/Learning JavaScript/LearningJavaScript.js
+++ JavaScript/Learning JavaScript/LearningJavaScript.js
@@ -8,3 +8,42 @@ for (let i = 0; i < 5; i++) {
        console.log('Yo! ', i);
     }, 1000);
  }
+ var x = 3;
+ x += 3;
+ x = 'truong';
+ alert("x + y = " + x);
+ var arr = ["apple", "banana", "lemon"];
+
+ var func = function sumOfTwoNumbers(a, b) {
+    return a + b;
+ }
+ document.getElementById("checkType").innerHTML = "The type is: " + typeof func;
+
+ var person = {
+    firstName : "Truong",
+    lastName : "Pham",
+    age : 21,
+    address : "HCM",
+    goToSchool : function() {
+        return "by bike";
+    },
+    getFullName : function() {
+        return this.firstName + " " + this.lastName;
+    }
+ }
+ delete person.age;
+
+ person.fullName = person.firstName + " " + person.lastName;
+
+ person.firstName = "Tuan";
+ var t = 'address';
+
+ person.playGame = function() {
+    return "playing CS:GO";
+ };
+
+
+
+ var studentName = "TRUONG";
+ var brands = "Apple Samsung Oppo Xiaomi Huawei Oppo Bphone";
+ document.getElementById("objectPerson").innerHTML = brands.replace(/Oppo/g, "Google");
