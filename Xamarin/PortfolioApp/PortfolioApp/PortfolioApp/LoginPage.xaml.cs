2364e4bff56921a87cba407749eb72912d70e97c
HoangTruongSoftDev <hoangtruongpham73@gmail.com>
2024-02-06 01:17:12 -0500

    Update Project

---
+++ Xamarin/PortfolioApp/PortfolioApp/PortfolioApp/LoginPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PortfolioApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLogin_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(usernameEntry.Text))
+            {
+                labelErrorMessage.Text = "Your username is missing";
+                labelErrorMessage.IsVisible = true;
+                return;
+            }
+            if (string.IsNullOrEmpty(passwordEntry.Text))
+            {
+                labelErrorMessage.Text = "Your password is missing";
+                labelErrorMessage.IsVisible = true;
+                return;
+            }
+            if (usernameEntry.Text != "Truong" && passwordEntry.Text != "123")
+            {
+                labelErrorMessage.Text = "Your username or password is wrong\n\n Please enter the correct account: \n\t+ Username: Truong\n\t+ Password: 123";
+                labelErrorMessage.IsVisible = true;
+                return;
+            }
+            else
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+        }
+    }
+}
