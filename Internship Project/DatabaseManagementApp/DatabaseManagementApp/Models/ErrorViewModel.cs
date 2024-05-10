43bc8fa494f67c0af74275de9f86293efa6ad5b9
HoangTruongSoftDev <hoangtruongpham73@gmail.com>
2024-05-10 03:40:54 -0400

    update internship project

---
+++ Internship Project/DatabaseManagementApp/DatabaseManagementApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatabaseManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Internship Project/DatabaseManagementApp/DatabaseManagementApp/package.js
@@ -0,0 +1,20 @@
+{
+  "name": "package-complete",
+  "productName": "Canada Horizon Database",
+  "version": "1.0.0",
+  "description": "Hello World!",
+  "main": "Program.cs",
+  "scripts": {
+    "start": "electron Program.cs",
+    "package": "electron-builder build"
+  },
+  "author": "Hoang Truong Pham",
+  "license": "MIT",
+  "devDependencies": {
+    "electron": "^30.0.2",
+    "electron-builder": "^24.13.3"
+  },
+  "build": {
+    "appId": "com.Truong.CanadaHorizonDatabase"
+  }
+}
