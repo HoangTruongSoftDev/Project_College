cbcf04edc5e273b9bc1641ccd3c808b0e89b8fa1
HoangTruongSoftDev <hoangtruongpham73@gmail.com>
2024-05-09 22:48:25 -0400

    initialize project internship

---
+++ Internship Project/LauncherApp/LauncherApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LauncherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Path to your Electron project's executable file
+            string electronAppPath = @"C:\Truong\Internship Project\Canada_Horizon_Database\main.js";
+
+            try
+            {
+                // Launch the Electron project
+                Process.Start(electronAppPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error launching Electron project: {ex.Message}");
+            }
+        }
+    }
+}
