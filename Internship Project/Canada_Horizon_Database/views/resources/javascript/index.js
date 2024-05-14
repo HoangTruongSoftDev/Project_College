5e10a751ba652ae48d32071835ee4d93605bfa82
HoangTruongSoftDev <hoangtruongpham73@gmail.com>
2024-05-14 10:43:48 -0400

    update project

---
+++ Internship Project/Canada_Horizon_Database/views/resources/javascript/index.js
@@ -0,0 +1,20 @@
+let divElement = document.getElementById("content-admin");
+
+// Add event listener for click event
+divElement.addEventListener("click", function () {
+    window.location = "admin-page.html"
+});
+
+divElement = document.getElementById("content-employer");
+
+// Add event listener for click event
+divElement.addEventListener("click", function () {
+    window.location = "employer-page.html"
+});
+
+divElement = document.getElementById("content-worker");
+
+// Add event listener for click event
+divElement.addEventListener("click", function () {
+    window.location = "worker-page.html"
+});
